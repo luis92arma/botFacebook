9aaa68db4905138bdd8735ec7fbfacbe97b0d94c
luis92arma <luis.arma92@gmail.com>
2017-01-23 13:47:24 -0600

    funcion para enviar y recibir imbox

--- bot/views.py
+++ bot/views.py
@@ -1,4 +1,4 @@
-import json, random, re
+import json, random, re, requests
 from pprint import pprint
 from django.shortcuts import render
 from django.views.generic import View
@@ -8,8 +8,15 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
 #Access token
-PAGE_ACCES_TOKEN =''
+PAGE_ACCESS_TOKEN ='EAAE24wJsJdUBAAzJCzFZCXQXmofSPLDIOFTBbIghUdRhqsDBfV8ZBLXY5vI3VhipXiLjV6HHxbwfV7cwlU0hmN7GLBL0vPkToZAxZBbKajEaJIvowi9VyZBolFrygH8tZC9cqAXl9DhMzwTUbW9TOjGLewsAaJiKuYa34iQa3BngZDZD'
 VERIFY_TOKEN = '961966336'
+def post_facebook_messages(fbid, recived_message):
+    #Cuando se manda llamar envia el mismo texto que se recibe
+    post_messages_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+    response_msg = json.dumps({'recipient':{'id':fbid}, 'message':{'text':recived_message}})
+    status = requests.post(post_messages_url, headers={'Content-Type':'application/json'},data=response_msg)
+    pprint(status.json())
+
 class BotMessenger(View):
     """docstring forBotMessenger"""
     def get(self, request):
@@ -29,4 +36,5 @@ class BotMessenger(View):
             for message in entry['messaging']:
                 if 'message' in message:
                     pprint(message)
+                    post_facebook_messages(message['sender']['id'], message['message']['text'])
         return HttpResponse()
